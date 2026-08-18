5f2dfb3ab7a4cc88cd85dafb4705d1d53e244a2a
HyperNova9 <60463021+HyperNova9@users.noreply.github.com>
2026-08-18 12:11:24 +0300

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.cs
@@ -0,0 +1,35 @@
+public class Solution 
+{
+    public int Trap(int[] height) 
+    {
+        int n = height.Length;
+        int left = 0, right = n - 1;
+        int water = 0;
+        var left_max = height[left];
+        var right_max = height[right];        
+        while (left < right)
+        {
+            if (left_max < right_max)
+            {
+                left++;
+                if (left_max < height[left])
+                    left_max = height[left];
+                int current = Math.Min(left_max, right_max) - height[left];
+                if (current > 0)
+                    water += current;
+                
+            }
+            else
+            {
+                right--;
+                if (right_max < height[right])
+                    right_max = height[right];
+                int current = Math.Min(left_max, right_max) - height[right];
+                if (current > 0)
+                    water += current;
+            }
+
+        }
+        return water;
+    }   
+}
