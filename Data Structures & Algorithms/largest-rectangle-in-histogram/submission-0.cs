8424864d4a097b68ac6da14efb5c7898f201acd0
HyperNova9 <60463021+HyperNova9@users.noreply.github.com>
2026-08-30 20:37:27 +0300

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-0.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int LargestRectangleArea(int[] heights) {
+        int n = heights.Length;
+        var stack = new Stack<int>();
+        var max_S = 0;
+        var count = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (stack.Count > 0 && heights[stack.Peek()] > heights[i]) {
+                int h = heights[stack.Pop()];
+                var left = stack.Count > 0 ? stack.Peek() : -1;
+                int w = i - left - 1;
+                max_S = Math.Max(max_S, h * w);
+            }
+            stack.Push(i);
+        }
+        while (stack.Count > 0) {
+            int h = heights[stack.Pop()];
+            var left = stack.Count > 0 ? stack.Peek() : -1;
+            int w = n - left - 1;
+            max_S = Math.Max(max_S, h * w);
+        }
+        return max_S;
+    }
+}
